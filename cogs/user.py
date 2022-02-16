2222a641aca4fab8db9c1a34f9adadaa39cdd76e
Brandon Frederick <1ultimatedj@gmail.com>
2022-02-16 21:22:59 +0000

    Working on dialogue structure and functionality. Dialogue uses eval. Need to add username check for (), {}, [], =, ., ,, and _. Stable.

--- cogs/user.py
+++ cogs/user.py
@@ -2,7 +2,7 @@ import random
 
 import discord
 
-from cogs.utils.embed_tpl import error_tpl
+from cogs.utils.embed_tpl import error_tpl, dialogue_tpl
 from cogs.utils.time import now
 from config import jdata
 from discord.ext import commands
@@ -15,9 +15,55 @@ class User(commands.Cog):
         self.bot = bot
 
 
+    async def send_dlg(self, ctx):
+        user_id = str(ctx.author.id)
+        if user_id in db['users'].keys():
+            user_dlg_id = db['users'][user_id]['dialogue_id']
+            en_chosen_dlg = jdata['en']['dialogue'][user_dlg_id['major']][user_dlg_id['minor']][user_dlg_id['sub']]
+        
+            if 'description' in en_chosen_dlg.keys() and 'author' in en_chosen_dlg.keys():
+                await ctx.send(embed=dialogue_tpl(eval(en_chosen_dlg['author']), eval(en_chosen_dlg['description'])))
+                print(f'{now()}: [{user_id}] Sent dialogue.')
+
+                jdata_chosen_dlg = jdata['game_data']['dialogue'][user_dlg_id['major']][user_dlg_id['minor']][user_dlg_id['sub']]
+                print(f'{now()}: [{user_id}] Defined jdata_chosen_dlg')
+                
+                if 'input' not in jdata_chosen_dlg.keys():
+                    print(f'{now()}: [{user_id}] Entered if scope.')
+                    
+                    db['users'][user_id]['dialogue_id']['major'] = jdata_chosen_dlg['next']['dialogue']['major']
+                    db['users'][user_id]['dialogue_id']['minor'] = jdata_chosen_dlg['next']['dialogue']['minor']
+                    db['users'][user_id]['dialogue_id']['sub'] = jdata_chosen_dlg['next']['dialogue']['sub']
+                    print(f'{now()}: [{user_id}] Updated major, minor, and sub.')
+                    
+                    await self.send_dlg(ctx)
+                    print(f'{now()}: [{user_id}] Done send recursion.')
+            else:
+                await ctx.send(embed=error_tpl(ctx, jdata[jdata['config']['chosen_language']]['errors']['dlg_no_desc_or_author']))
+        else:
+            await ctx.send(embed=error_tpl(ctx, jdata[jdata['config']['chosen_language']]['errors']['not_registered']))
+
+    
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{now()}: User cog is online.')
+
+
+    @commands.command(aliases=['i', 'inv', 'data'])
+    async def info(self, ctx):
+        user_id = str(ctx.author.id)
+        if user_id not in db['users'].keys():
+            await ctx.send(embed=error_tpl(ctx, jdata[jdata['config']['chosen_language']]['errors']['not_registered']))
+            return
+
+        user_data = db['users'][user_id]
+        embed = discord.Embed(
+            description=f'Quarx: {user_data["quarx"]}\nShips: {len(user_data["ships"])}\n Colonies: {len(user_data["colonies"])}',
+            color=int(jdata['config']['colors']['info'], 16))
+        embed.set_author(name=f'[{user_data["username"]}] Personal Data.', icon_url=jdata['config']['icons']['success'])
+        embed.set_footer(text=f'This data will expire in {jdata["config"]["delete_after"]["default"]} seconds.')
+        await ctx.message.delete()
+        await ctx.send(embed=embed, delete_after=jdata['config']['delete_after']['default'])
 
 
     @commands.command(aliases=['join', 'register'])
@@ -40,42 +86,29 @@ class User(commands.Cog):
                 'ships': {},
                 'colonies': {},
                 'dialogue_id': {
-                    'major': 0,
-                    'minor': 0,
-                    'sub': 0
+                    'major': '0',
+                    'minor': '0',
+                    'sub': '0'
                 },
                 'occupation': '',
                 'skills': {}
             }
             db['users'][str(ctx.author.id)] = user_data
-            
-            embed = discord.Embed(color=int(jdata['config']['colors']['info'], 16))
-            embed.set_author(name=f'[{user_data["username"]}] Done starting.', icon_url=jdata['config']['icons']['success'])
-            embed.set_footer(text=f'Quarx: {user_data["quarx"]}\nShips: {len(user_data["ships"].keys())} | Colonies: {len(user_data["colonies"].keys())}')
-            await ctx.send(embed=embed)
+
+            await self.send_dlg(ctx)
             return
+            
         await ctx.send(embed=error_tpl(ctx, jdata[jdata['config']['chosen_language']]['errors']['already_registered']))
 
-
-    @commands.command(aliases=['i', 'inv', 'data'])
-    async def info(self, ctx):
-        user_id = str(ctx.author.id)
-        if user_id not in db['users'].keys():
-            await ctx.send(embed=error_tpl(ctx, jdata[jdata['config']['chosen_language']]['errors']['not_registered']))
-            return
-
-        user_data = db['users'][user_id]
-        embed = discord.Embed(
-            description=f'Quarx: {user_data["quarx"]}\nShips: {len(user_data["ships"])}\n Colonies: {len(user_data["colonies"])}',
-            color=int(jdata['config']['colors']['info'], 16))
-        embed.set_author(name=f'[{user_data["username"]}] Personal Data.', icon_url=jdata['config']['icons']['success'])
-        embed.set_footer(text=f'This data will expire in {jdata["config"]["delete_after"]["default"]} seconds.')
-        await ctx.message.delete()
-        await ctx.send(embed=embed, delete_after=jdata['config']['delete_after']['default'])
-
-
+    
     @commands.command(aliases=['in'])
     async def input(self, ctx, input):
+        
+        return
+
+
+    @commands.command(aliases=['dlg'])
+    async def dialogue(self, ctx, input):
         
         return
 
--- cogs/utils/embed_tpl.py
+++ cogs/utils/embed_tpl.py
@@ -1,9 +1,22 @@
 import discord
 
 from config import jdata
+from replit import db
 
 
 def error_tpl(ctx, desc):
+    user_id = str(ctx.author.id)
+    
     embed = discord.Embed(description=desc, color=int(jdata['config']['colors']['error'], 16))
-    embed.set_author(name=f'[{ctx.author.name}] Error', icon_url=jdata['config']['icons']['error'])
+    embed.set_author(name=f'[{db["users"][user_id]["username"]}] Error', icon_url=jdata['config']['icons']['error'])
+    return embed
+
+def dialogue_tpl(author, desc):
+    if desc == '':
+        embed = discord.Embed(color=int(jdata['config']['colors']['error'], 16))
+    else:
+        embed = discord.Embed(description=desc, color=int(jdata['config']['colors']['error'], 16))
+        
+    if author != '':
+        embed.set_author(name=author)
     return embed
