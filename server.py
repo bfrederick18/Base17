fa363fc3bc6578331962466121aea7f0773997b3
Brandon Frederick <1ultimatedj@gmail.com>
2022-03-14 00:04:37 +0000

    Working on server. Stable.

--- server.py
+++ server.py
@@ -1,14 +1,31 @@
+import logging
+
+from cogs.utils.time import now
 from flask import Flask
 from threading import Thread
 
+
 app = Flask('')
+app.logger.disabled = True
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 
 @app.route('/')
 def home():
     return "o.o"
 
+
+@app.after_request
+def after_request_func(response):
+    print(f'{now()}: Request: "{response}"')
+    return response
+
+
 def run():
   app.run(host='0.0.0.0',port=8080)
+
 
 def keep_alive():
     t = Thread(target=run)
